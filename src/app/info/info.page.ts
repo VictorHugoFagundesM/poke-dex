1cb172428d43fbe789dea8a3c55a54548539284c
Victor Fagundes <victorhugofmiranda@gmail.com>
2023-10-08 22:12:19 -0300

    Finalização da tela de informações, início do desenvolvimento da tela de favoritos

--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -1,4 +1,8 @@
+import { StorageService } from './../services/storage/storage.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { PokemonType } from 'src/enums/pokemon-type.enum';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-info',
@@ -7,9 +11,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoPage implements OnInit {
 
-  constructor() { }
+  public isFavorite:boolean = false;
+  private favoriteList:Array<any> = [];
+  public pokemonId:any;
+  public pokemonData:any;
+  public pokemonType:any = PokemonType;
+  public statsData:Array<any> = [
+    {
+      name: "HP",
+      min: 1,
+      max: 255,
+      with: 0
+    },
+    {
+      name: "Attack",
+      min: 5,
+      max: 190,
+      with: 0
+    },
+    {
+      name: "Defense",
+      min: 10,
+      max: 230,
+      with: 0
+    },
+    {
+      name: "Sp. Atk",
+      min: 10,
+      max: 194,
+      with: 0
+    },
+    {
+      name: "Sp. Def",
+      min: 20,
+      max: 230,
+      with: 0
+    },
+    {
+      name: "Speed",
+      min: 5,
+      max: 180,
+      with: 0
+    },
+  ];
+
+  constructor(
+    private route: ActivatedRoute,
+    private storage: StorageService,
+    public location: Location
+  ) { }
 
   ngOnInit() {
+
+    this.pokemonId = this.route.snapshot.paramMap.get('id');
+
+    if (this.pokemonId) {
+
+      // Obtém os do pokemon no storage
+      this.storage.get("pokemon").then((data:any) => {
+        // @ts-ignore
+        this.pokemonData = data[this.pokemonId -1];
+      });
+
+      // Obtém a lista dos favoritos no storage
+      this.storage.get("favorites").then((data:any) => {
+        if (data) {
+          this.favoriteList = data;
+        }
+      });
+
+    }
+
+  }
+
+  /**
+   * Adiciona o pokemon à lista de favoritos
+   */
+  addFavoritePokemon() {
+
+    // Remove a informação de favoritos no storage
+    if (this.isFavorite) {
+      this.favoriteList.pop();
+
+    // Guarda a informação de favoritos no storage
+    } else {
+      this.favoriteList.push(this.pokemonId);
+    }
+
+    this.storage.set("favorites", this.favoriteList);
+    this.isFavorite = !this.isFavorite;
+
   }
 
 }
